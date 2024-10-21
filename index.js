0580feb70f59555b8bc58bf1a92f2e8d5c39f0e1
GJRC23 <gastuconsoli8@gmail.com>
2024-10-21 12:33:05 -0300

    CORS para que funcione

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware de CORS
-app.use(cors({
-  origin: '*', // Reemplaza con la URL de tu frontend
-  credentials: true, // Permitir envío de cookies y cabeceras autorizadas
-}));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+// Config CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
 // Conectar a la base de datos
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Conectado a MongoDB'))
-  .catch(err => console.error('Error al conectar a MongoDB', err));
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Conectado a MongoDB"))
+  .catch((err) => console.error("Error al conectar a MongoDB", err));
 
 // Rutas
-const productRoutes = require('./routes/productRoutes');
-app.use('/api/products', productRoutes);
+const productRoutes = require("./routes/productRoutes");
+app.use("/api/products", productRoutes);
 
 // Iniciar servidor
 app.listen(PORT, () => {
