b883f6e826ca4acb91905e718ba28669708a3a52
GJRC23 <gastuconsoli8@gmail.com>
2024-10-21 12:45:02 -0300

    nueva config cors

--- index.js
+++ index.js
@@ -15,13 +15,17 @@ app.use(
   })
 );
 
-// Config CORS
+// Configuración de cabeceras para CORS
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200); // Manejar las solicitudes preflight de CORS
+  }
   next();
 });
 
